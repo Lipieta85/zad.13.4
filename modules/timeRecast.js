b3d1be7501ca00614c9226980cb117f4840dc694
Rafał Lipieta <cwgflojdid@wp.pl>
2018-06-20 18:40:22 +0200

    timeRecast

---
+++ modules/timeRecast.js
@@ -0,0 +1,14 @@
+var os = require('os');
+
+function timeRecast() {
+
+    var uptime = Math.floor(os.uptime());
+    var hours = Math.floor(uptime / 3600);
+    var minutes = Math.floor((uptime - (hours * 3600)) / 60);
+    
+    uptime = uptime % 60;
+    
+	return hours + ' hours ' + minutes + ' minutes ' + uptime + ' seconds'
+}
+
+exports.time = timeRecast;
